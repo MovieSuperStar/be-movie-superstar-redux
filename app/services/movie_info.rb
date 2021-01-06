10852123ae96860a25205deb082ca7b25d2b5a61
Joshua O'Bannon <jtobannon@gmail.com>
2021-01-06 00:18:27 -0700

    WIP caching

--- app/services/movie_info.rb
+++ app/services/movie_info.rb
@@ -16,10 +16,11 @@ class MovieInfo
   private
 # "#{@query_params}"
   def get_json
-    response = Rails.cache.fetch("#{@query_params}", expires_in: 59.minutes) do
-      Faraday.get("http://www.omdbapi.com?#{@query_params}&apikey=#{ENV['OMDB_KEY']}")
+    Rails.cache.fetch("#{@query_params}", expires_in: 59.minutes) do
+      response = Faraday.get("http://www.omdbapi.com?#{@query_params}&apikey=#{ENV['OMDB_KEY']}")
+      JSON.parse(response)
     end
-    JSON.parse(response.body)
+    # JSON.parse(response.body)
   end
 
   def add_count_other(raw_json)
@@ -27,10 +28,11 @@ class MovieInfo
       find_vote = Vote.find_by(imdb_id: movie['imdbID'])
       find_vote.nil? ? movie['count'] = 0 : movie['count'] = find_vote.count
       @movie_id = movie['imdbID']
-      response = Rails.cache.fetch("#{@movie_id}", expires_in: 59.minutes) do
-        Faraday.get("http://www.omdbapi.com?i=#{@movie_id}&apikey=#{ENV['OMDB_KEY']}")
+      Rails.cache.fetch("#{@movie_id}", expires_in: 59.minutes) do
+        response = Faraday.get("http://www.omdbapi.com?i=#{@movie_id}&apikey=#{ENV['OMDB_KEY']}")
+        movie['details'] = JSON.parse(response.body)
+        # JSON.parse(response.body)
       end
-      movie['details'] = JSON.parse(response.body)
     end
   end
 
